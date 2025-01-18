3672d6ce880477243db00c3be2b68647b4e5739e
Tatianaob <ltatiobandore@gmail.com>
2025-01-17 16:43:45 -0800

    Added commands

--- system_info.sh
+++ system_info.sh
@@ -9,5 +9,21 @@ echo -e "\nCurrent User: $(whoami)"
 echo -e "\nSystem Uptime: "
 Uptime
 
+echo -e "\n Memory Usage:"
+free -h
+
+echo -e "\nDisk Usage:"
+df -h
+
+echo -e "\nTop 5 processes by memory usage: "
+ps aux --sort=-%mem | head -n 6
+
+echo -e "\nNetwork Information"
+ip -c a
+
+echo -e "\n=================="
+echo "End of Report"
+echo "Have a good weekend"
+echo "*******==============******"
 
 

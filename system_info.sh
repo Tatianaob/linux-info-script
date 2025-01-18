ab69e4a580cf953ef41e863c1d232cb03c3abb28
Tatianaob <ltatiobandore@gmail.com>
2025-01-17 16:30:19 -0800

    Create linux dashboard

---
+++ system_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "==========="
+echo "Welcome to the Linux Info Dashboard"
+echo "==========="
+
+echo -e "\nCurrent User: $(whoami)"
+
+echo -e "\nSystem Uptime: "
+Uptime
+
+
+
